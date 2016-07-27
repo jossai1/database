f25a039050a071fbe55cb9802d8908ce8cec1385
jossai1 <buff_jane@hotmail.com>
2016-07-27 15:44:52 +0100

    Update server.js
    added cors

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ var bodyParser = require('body-parser');
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+res.set('Access-Control-Allow-Origin', '*');
+res.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+next();
+});
 
 var port = process.env.PORT || 8095;        // set our port
 
